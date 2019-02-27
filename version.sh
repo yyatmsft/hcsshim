fa2403b0bfbadb50004be1bcfdbce0e752bacfeb
Kevin Parsons <kevpar@microsoft.com>
2019-02-27 18:39:04 +0000

    Add cdpx prefix to version string
    Adding this prefix will prevent the tags created by cdpx from matching the
    `git describe` pattern, which would have created a case of weirdly "nesting"
    tags (e.g. v1.0-YYYYMMDD-NN, v1.0-YYYYMMDD-NN-YYYYMMDD-NN, etc.).

--- version.sh
+++ version.sh
@@ -6,6 +6,6 @@ cd /source
 
 gitVersion=$(git describe --match 'v[0-9]*' --always --long --tags)
 timeStamp=$(date --utc +%Y%m%d)
-version=${gitVersion}-${timeStamp}-${CDP_DEFINITION_BUILD_COUNT}
+version=cdpx-${gitVersion}-${timeStamp}-${CDP_DEFINITION_BUILD_COUNT}
 
 echo '##vso[build.updatebuildnumber]'$version
