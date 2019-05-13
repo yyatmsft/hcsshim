c8ace0934ce20077205a50cc0209ea6a3da8cf62
John Starks <jostarks@microsoft.com>
2019-05-13 17:27:58 +0000

    Build using modules (instead of GOPATH)

--- build.sh
+++ build.sh
@@ -6,19 +6,16 @@ BASH_XTRACEFD=1
 set -eux
 
 export GOOS=windows
-export GOPROXY=off # Prohibit downloads
+export GOPROXY=off         # Prohibit downloads
+export GOFLAGS=-mod=vendor # Build using vendor directory
 
 mkdir -p /source/cdpx-artifacts
 cd /source/cdpx-artifacts
 
-# Set up GOPATH with a symlink pointing to the actual source location.
-mkdir -p $GOPATH/src/github.com/Microsoft
-ln -s /source $GOPATH/src/github.com/Microsoft/hcsshim
-
-go build -v github.com/Microsoft/hcsshim/cmd/containerd-shim-runhcs-v1
-go build -v github.com/Microsoft/hcsshim/cmd/runhcs
-go build -v github.com/Microsoft/hcsshim/cmd/tar2ext4
-GOOS=linux go build -v github.com/Microsoft/hcsshim/cmd/tar2ext4
-go build -v github.com/Microsoft/hcsshim/internal/tools/zapdir
-go build -v github.com/Microsoft/hcsshim/internal/tools/grantvmgroupaccess
-go test -c -v github.com/Microsoft/hcsshim/test/cri-containerd --tags functional
+go build github.com/Microsoft/hcsshim/cmd/containerd-shim-runhcs-v1
+go build github.com/Microsoft/hcsshim/cmd/runhcs
+go build github.com/Microsoft/hcsshim/cmd/tar2ext4
+GOOS=linux go build github.com/Microsoft/hcsshim/cmd/tar2ext4
+go build github.com/Microsoft/hcsshim/internal/tools/zapdir
+go build github.com/Microsoft/hcsshim/internal/tools/grantvmgroupaccess
+go test -c github.com/Microsoft/hcsshim/test/cri-containerd --tags functional
