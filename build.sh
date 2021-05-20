2e47066c1a129c33825f53ee72bf6621c86ab836
Kathryn Baldauf <kabaldau@microsoft.com>
2021-05-20 13:52:06 -0700

    Add a build command for the jobobject-util tool
    Signed-off-by: Kathryn Baldauf <kabaldau@microsoft.com>

--- build.sh
+++ build.sh
@@ -17,6 +17,7 @@ go build github.com/Microsoft/hcsshim/cmd/runhcs
 go build github.com/Microsoft/hcsshim/cmd/shimdiag
 go build github.com/Microsoft/hcsshim/cmd/tar2ext4
 go build github.com/Microsoft/hcsshim/cmd/device-util
+go build github.com/Microsoft/hcsshim/cmd/jobobject-util
 GOOS=linux go build -buildmode=pie github.com/Microsoft/hcsshim/cmd/tar2ext4
 go build github.com/Microsoft/hcsshim/internal/tools/zapdir
 go build github.com/Microsoft/hcsshim/internal/tools/grantvmgroupaccess
